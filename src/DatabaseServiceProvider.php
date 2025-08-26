e5e2a0dcee66fd1c926b3a6184cae53aeaa02540
uturakulov <uturakulov@paymart.uz>
2025-08-26 10:59:26 +0500

    revert changes

--- src/DatabaseServiceProvider.php
+++ src/DatabaseServiceProvider.php
@@ -20,31 +20,10 @@ class DatabaseServiceProvider extends ServiceProvider
             if (config('prometheus.collect_full_sql_query')) {
                 $querySql = $this->cleanupSqlString((string)$query->sql);
             }
-            
-            $labels = [
-                $querySql ?: '[omitted]',
-                $type ?: 'UNKNOWN'
-            ];
-
-            // если включён флаг, ищем сервис
-            if (config('prometheus.collect_sql_service_caller')) {
-                $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 50);
-
-                $serviceCaller = '[unknown]';
-                foreach ($trace as $frame) {
-                    if (!isset($frame['class'])) {
-                        continue;
-                    }
-
-                    if (str_starts_with($frame['class'], 'App\\Services\\')) {
-                        $serviceCaller = $frame['class'] . (isset($frame['function']) ? '::' . $frame['function'] : '');
-                        break;
-                    }
-                }
-
-                $labels[] = $serviceCaller;
-            }
-
+            $labels = array_values(array_filter([
+                $querySql,
+                $type
+            ]));
             $this->app->get('prometheus.sql.histogram')->observe($query->time, $labels);
         });
     }
@@ -57,19 +36,13 @@ class DatabaseServiceProvider extends ServiceProvider
     public function register() : void
     {
         $this->app->singleton('prometheus.sql.histogram', function ($app) {
-            $labelNames = [
-                'query',
-                'query_type',
-            ];
-
-            if (config('prometheus.collect_sql_service_caller')) {
-                $labelNames[] = 'service_caller';
-            }
-
             return $app['prometheus']->getOrRegisterHistogram(
                 'sql_query_duration',
                 'SQL query duration histogram',
-                $labelNames,
+                array_values(array_filter([
+                    'query',
+                    'query_type'
+                ])),
                 config('prometheus.sql_buckets') ?? null
             );
         });
